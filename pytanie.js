5e41403eb863e850c58b452cc70764d08a62c121
jakub-zyrek <jakubzyrek0@gmail.com>
2023-03-20 21:06:08 +0100

    20.03 21:06

---
+++ pytanie.js
@@ -0,0 +1,26 @@
+var i = 0;
+
+function coment() {
+    if (i == 0) {
+        document.getElementById('kome').innerHTML = "<div class='card-body' style='padding-left: 5%; background-color: white; border-radius: 1vw;'><div style='margin-left: 3%;'><p><b>Jakub Żyrek:</b>&nbsp;&nbsp; Byczes dlaczego</p><p><b>Maria Dazdd`ur:</b>&nbsp;&nbsp; Elo mordo</p><form action='' method='post' class='needs-validation col-12' novalidate><br><label for='kom' class='form-label col-12' style='width: 100%; margin: 0%;display: flex; align-items: center; justify-content: center;'><b style='width: 24%;'>Jakub Żyrek:</b><input type='text' class='form-control' id='kom' placeholder='Wprowadź komentarz' required style='border: none; border-bottom: black 1px solid; border-radius: 0px; outline: none; width: 60%;'><button type='submit' class='btn btn-outline-dark text-center' style='width: 10%; margin-left: 2%; padding: 1%;'><svg xmlns='http://www.w3.org/2000/svg' style='width: 1rem;' fill='currentColor' class='bi bi-send-fill' viewBox='0 0 16 16'><path d='M15.964.686a.5.5 0 0 0-.65-.65L.767 5.855H.766l-.452.18a.5.5 0 0 0-.082.887l.41.26.001.002 4.995 3.178 3.178 4.995.002.002.26.41a.5.5 0 0 0 .886-.083l6-15Zm-1.833 1.89L6.637 10.07l-.215-.338a.5.5 0 0 0-.154-.154l-.338-.215 7.494-7.494 1.178-.471-.47 1.178Z'/></svg></button></label><div class='invalid-feedback'>Nic nie wpisano</div></form></div></div>";
+
+        document.getElementById('komet').innerText = " Ukryj komentarze";
+        i = 1;
+    } else {
+        document.getElementById('kome').innerHTML = "";
+        document.getElementById('komet').innerText = " Pokaż komentarze";
+        i = 0;
+    }
+}
+
+var a = 0;
+
+function dzieki() {
+    if (a == 0) {
+        document.getElementById('dzieki').innerText = "Podziękowałeś";
+        a = 1;
+    } else {
+        document.getElementById('dzieki').innerText = "Podziękuj";        
+        a = 0;
+    }
+}
